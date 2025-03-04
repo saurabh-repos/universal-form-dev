6c8655dd67c1169811f302aff66e0b51abb2aa08
Saurabh Pandey <saurabhpandey616@gmail.com>
2025-03-04 17:03:51 +0530

    updated

--- src/app/store/slices/menuSlice.js
+++ src/app/store/slices/menuSlice.js
@@ -12,12 +12,9 @@ const menuSlice = createSlice({
   reducers: {
     setActiveMainMenu: (state, action) => {
       state.activeMainMenu = action.payload;
-
-      // Find menu ID based on name
       const selectedMenu = MAIN_MENU_ITEMS.find((item) => item.name === action.payload);
 
       if (selectedMenu) {
-        // Find the first sidebar item related to the menu
         const firstSidebarItem = SIDEBAR_ITEMS.find(item => item.menuId === selectedMenu.id);
 
         state.activeSidebarItem = firstSidebarItem ? firstSidebarItem.path : "";
